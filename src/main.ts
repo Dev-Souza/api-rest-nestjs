b1edda0b7e196fa6d23d0f30e2b3fc31f7cfd840
Dev-Souza <kauan.souza.dev@gmail.com>
2024-11-20 12:40:19 -0300

    feat: Configuração de porta e CORS usando middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(
+    cors({
+      origin: 'http://localhost:3000', // Permite a origem do front-end
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    }),
+  );
+  await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
